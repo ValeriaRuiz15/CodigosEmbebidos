967c31ad68a213ce2d4c0e8a81db3766d30dbc01
valeria <valeyutsil2@gmail.com>
2025-09-29 15:55:23 -0600

    v2

--- PracticaEmbebidos.py
+++ PracticaEmbebidos.py
@@ -3,19 +3,19 @@ import time
 gpio.setmode(gpio.BOARD)
 
 #Pines 
-a1 = 1
-a2 = 1
-r1 = 1
-r2 = 1
-v1 = 1
-v2 = 1
+a1 = 3
+a2 = 8
+r1 = 5
+r2 = 10
+v1 = 7
+v2 = 12
 
-pv1 = 1
-pr1 = 1
-pv2 = 1
-pr2 = 1
+pv1 = 35
+pr1 = 37
+pv2 = 36
+pr2 = 38
 
-boton = 1
+boton = 13
 
 gpio.setup(v1,gpio.OUT) #verde1
 gpio.setup(a1,gpio.OUT) #amarillo1
@@ -33,7 +33,7 @@ gpio.setup(pr2,gpio.OUT) #pr2
 
 gpio.setup(boton,gpio.IN) #boton
 
-
+#Todo apagado
 gpio.output(a1,0)
 gpio.output(v1,0)
 gpio.output(r1,0)
@@ -48,9 +48,11 @@ gpio.output(pr1,0)
 gpio.output(pv2,0)
 gpio.output(pr2,0)
 
+#Peatonal
 estado = 0
 
 def peatonal():
+    #rojos autos
     gpio.output(a1,0)
     gpio.output(v1,0)
     gpio.output(r1,1)
@@ -59,6 +61,7 @@ def peatonal():
     gpio.output(v2,0)
     gpio.output(r2,1)
 
+    #verdes peatonal
     gpio.output(pv1,1)
     gpio.output(pr1,0)
 
@@ -66,8 +69,9 @@ def peatonal():
     gpio.output(pr2,0)
     time.sleep(5)
 
-    estado=0
+    #estado=0 
 
+    #apaga autos
     gpio.output(a1,0)
     gpio.output(v1,0)
     gpio.output(r1,0)
@@ -76,6 +80,7 @@ def peatonal():
     gpio.output(v2,0)
     gpio.output(r2,0)
 
+    #rojo peatonal
     gpio.output(pv1,0)
     gpio.output(pr1,1)
 
@@ -84,9 +89,8 @@ def peatonal():
 
 
 
-
 while True:
-
+    #Apaga todos
     gpio.output(a1,0)
     gpio.output(v1,0)
     gpio.output(r1,0)
@@ -98,44 +102,47 @@ while True:
     gpio.output(pv1,0)
     gpio.output(pr1,0)
 
+    #rojo peatonal
     gpio.output(pr1,1)
     gpio.output(pr2,1)
 
     t1 = time.time()
 
     while(time.time()-t1 <= 5):
-        gpio.output(v1,1)
-        gpio.output(r2,1)
+        gpio.output(v1,1) # verde sem1
+        gpio.output(r2,1) #rojo sem2
 
-        if(gpio.input(boton)==True):
+        if(gpio.input(boton)): #si presiona cambia 
             estado=1
-    
 
+    #parpadeo de verde autos
     for i in range(3):
         gpio.output(v1,1)
         time.sleep(0.3)
         gpio.output(v1,0)
+        time.sleep(0.3)
 
 
     t1 = time.time()
 
-    if (estado==1):
+    if (estado==1): #peatonal
         peatonal()
+        estado = 0 
 
 
     while(time.time()-t1 <= 3):
-        gpio.output(a1,1)
-        if(gpio.input(boton)==True):
+        gpio.output(a1,1) #amarillo sem1
+        if(gpio.input(boton)): #cambio de estado
             estado=1
 
-    gpio.output(a1,0)
+    gpio.output(a1,0) 
 
     t1 = time.time()
     while(time.time()-t1 <= 5):
         gpio.output(v2,1)
         gpio.output(r1,1)
 
-        if(gpio.input(boton)==True):
+        if(gpio.input(boton)):
             estado=1
 
 
@@ -143,19 +150,19 @@ while True:
         gpio.output(v2,1)
         time.sleep(0.3)
         gpio.output(v2,0)
+        time.sleep(0.3)
 
     if (estado==1):
         peatonal()
+        estado=0
 
 
     t1 = time.time()
     while(time.time()-t1 <= 3):
         gpio.output(a2,1)
-        if(gpio.input(boton)==True):
+        if(gpio.input(boton)):
             estado=1
 
 
 
-
-        
 
